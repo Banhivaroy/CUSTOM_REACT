d0fcaedc246856a5bec2fea116be1904483b5b44
Banhivaroy <roybanhiva@gmail.com>
2025-08-22 22:31:58 +0530

    commited

---
+++ CustomReact.js
@@ -0,0 +1,30 @@
+
+// function customRender(ele,container){
+//     const domele = document.createElement(ele.type);
+//     domele.innerHTML = ele.children;
+//     domele.setAttribute('href',ele.props.href);
+//     domele.setAttribute('target',ele.props.target);
+//     container.appendChild(domele);
+// }
+
+const mainContainer = document.getElementById("root");
+const domele = document.createElement(element.type);
+domele.innerHTML = element.children;
+for (const key in element.props) {
+    if(key === 'children') continue;
+    domele.setAttribute(key,element.props.props.key)
+}
+mainContainer.appendChild(domele);
+
+
+
+const element = { // it creates an <a></a> tag in this js file since it is linked with the html file it will be showed in the webPage
+    type: 'a',
+    props:{
+         href: 'https://google.com',
+         target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+// customRender(element,mainContainer) // two attributes -> which element to create & where to create
